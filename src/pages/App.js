52d1e408cce267216df06e33fc9f15e840ddb944
musembi77 <sammymusembi77@gmail.com>
2021-11-30 10:26:59 +0300

    error fix commit

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React from 'react'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Home from './Home'
 import Explore from './Explore';
